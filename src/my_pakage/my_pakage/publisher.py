4a3c916e98c5e314776db3a599dbd541fdb4c370
Efrieza <senricbe@gmail.com>
2026-04-06 12:58:57 +0200

    commit inicial 0.1

---
+++ src/my_pakage/my_pakage/publisher.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class MyPublisher(Node):
+    def __init__(self):
+        super().__init__('my_publisher')
+        self.publisher = self.create_publisher(String, 'my_topic', 10)
+        timer_period = 0.5 
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "messege # %d" %self.i
+        self.publisher.publish(msg)
+        self.get_logger().info("publoching: %s" % msg.data)
+        self.i +=1
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        publisher = MyPublisher()
+        rclpy.spin(publisher)
+    except KeyboardInterrupt:
+        print("exit node")
+    except Exception as e:
+        print(e)
+
+        
+if __name__ == '__main__':
+    main()
---
+++ src/my_pakage/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_pakage'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Efrieza',
+    maintainer_email='sernicbe@gmail.com',
+    description='Projecte TDR',
+    license='Mudle Catala',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_node = my_pakage.my_node:main',
+            'my_publisher = my_pakage.publisher:main',
+            'my_suscriber = my_pakage.suscriber:main'
+
+        ],
+    },
+)
