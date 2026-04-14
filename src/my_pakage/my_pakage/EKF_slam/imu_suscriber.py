f388b1ce20279a1fac1f8aa6981ffb6e1db292d4
Efrieza <senricbe@gmail.com>
2026-04-14 23:05:28 +0200

    0.3.0 primera versio de un EKF-slam

--- src/my_pakage/my_pakage/EKF_slam/imu_suscriber.py
+++ src/my_pakage/my_pakage/EKF_slam/imu_suscriber.py
@@ -1,6 +1,21 @@
 #node de suscrtipcio de un IMU per a trobar la posicio de un cotxe mitjançant Float 32MultiArray, el node es subscriu a un topic on el IMU publica les dades 
+#ax acceleracio en x
+#ay acceleracio en y
+#az acceleracio en z
+#vgx velocitat angular en x
+#vgy velocitat angular en y
+#vgz velocitat angular en z
+#gzx angle entre els eixos x i z
+#gzy angle entre els eixos y i z
+#gxy angle entre els eixos x i y
+#v velocitat del cotxe
+#axp acceleracio en x del cotxe produida pel pes del cotxe
+#ayp acceleracio en y del cotxe produida pel pes del cotxe
+#azp acceleracio en z del cotxe produida pel pes del cotxe
 
+#x i y posicio del cotxe en el pla, es calcula a partir de les dades de l'IMU i es publica en un topic per a que altres nodes puguin utilitzar aquesta informacio per a la navegacio del cotxe.
 import rclpy
+import math
 from rclpy.node import Node
 from my_pakage.msg import IMU
 
@@ -8,6 +23,7 @@ from my_pakage.msg import IMU
 class IMUSuscriber(Node):
     def __init__(self):
         super().__init__('imu_suscriber')
+        self.start_position
         #falta el publixher i el test
         self.imu_subscriver = self.create_subscription(
             IMU,
@@ -17,12 +33,38 @@ class IMUSuscriber(Node):
         )
         self.imu_publisher = self.create_publisher(IMU,'IMU_data',10)
 
+    def start_position(self, msg):
+
+        msg.gzx = (math.arccosine(msg.ax/msg.az))
+        msg.gzy = (math.arccosine(msg.ay/msg.az))
+        msg.gxy = (math.arccosine(msg.ax/msg.ay))
+
+        msg.axp = msg.ax
+        msg.ayp = msg.ay
+        msg.azp = msg.az
+
+        return msg.gzx, msg.gzy, msg.gxy, msg.axp, msg.ayp, msg.azp
+    
     def listener_callback(self, msg):
-        self.get_logger().info(f'received: ax={msg.ax}, ay={msg.ay}, vx={msg.vx}, vy={msg.vy}, gz={msg.gz}')
-        msg.vx = msg.vx + msg.ax * 0.1
-        msg.vy = msg.vy + msg.ay * 0.1
-        msg.x = msg.vx * 0.1
-        msg.y = msg.vy * 0.1
+
+        msg.gzx = msg.vgx*0.1 + msg.gzx
+        msg.gzy = msg.vgy*0.1 + msg.gzy
+        msg.gxy = msg.vgz*0.1 + msg.gxy
+        
+        msg.axp = math.cos(msg.gzx)*msg.axp + math.cos(msg.gzy)*msg.ayp + math.cos(msg.gxy)*msg.azp
+        msg.ayp = math.cos(msg.gzx)*msg.axp + math.cos(msg.gzy)*msg.ayp + math.cos(msg.gxy)*msg.azp
+        msg.azp = math.cos(msg.gzx)*msg.axp + math.cos(msg.gzy)*msg.ayp + math.cos(msg.gxy)*msg.azp
+
+        msg.v = (msg.ax-msg.axp)*0.1 + msg.v
+        msg.x = msg.v*math.cos(msg.gyx)
+        msg.y = msg.v*math.sin(msg.gyx)
+
+
+        
+
+
+        
+
 
         self.imu_publisher.publish(msg)
                 
