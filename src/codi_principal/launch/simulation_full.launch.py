c98aecf899d7291579e44ceaec4af87575c94801
Efrieza <senricbe@gmail.com>
2026-09-24 19:40:20 +0000

    preparar el merge

--- src/codi_principal/launch/simulation_full.launch.py
+++ src/codi_principal/launch/simulation_full.launch.py
@@ -34,9 +34,7 @@ def generate_launch_description():
         # increase LiDAR max_range and choose the complex map; set initial speed
         launch_arguments={
             'use_rviz': use_rviz,
-            'max_range': '5.0',
             'map_file': os.path.join(get_package_share_directory('car_simulator'), 'maps', 'complex_track_clean.yaml'),
-            'speed_mps': '0.5',
         }.items(),
     )
 
